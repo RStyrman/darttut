a6f1f1456dc3eb57f373c365295901ce0532ec94
rstyrman <richard.styrman@gmail.com>
2014-12-14 10:16:21 +0000

    Yo

---
+++ click_confirm.dart
@@ -0,0 +1,9 @@
+import 'dart:html';
+main() {
+    var button = new ButtonElement();
+    button..id = 'confirm'
+        ..text = 'Click to Confirm'
+        ..classes.add('important')
+        ..on.click.add((e) => window.alert('Confirmed!'));
+    query('#registration').elements.add(button);
+}
---
+++ distance.dart
@@ -0,0 +1,7 @@
+import 'dart:math';
+
+main() {
+  var p = new Point(2, 3);
+  var q = new Point(3, 4);
+  print('distance from p to q = ${p.distanceTo(q)}');
+}
